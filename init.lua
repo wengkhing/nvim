7adda71415c8f0b615937d56287a43717953ca58
WK Wong <wengkhing@WKs-MacBook-Pro.local>
2026-01-16 10:23:37 +0800

    auto switch project root

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+      vim.cmd("cd " .. vim.fn.argv(0))
+    end
+  end,
+})
